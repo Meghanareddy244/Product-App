ac50c537c63dc9f07993fda04638268d72bf54c9
Meghanareddy244 <meghanareddys999@gmail.com>
2025-09-27 08:08:58 +0530

    tailwind-config-changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false,
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",       // App Router pages
-    "./components/**/*.{js,ts,jsx,tsx}" // Components folder
+    "./app/**/*.{js,ts,jsx,tsx}", // App Router pages
+    "./components/**/*.{js,ts,jsx,tsx}", // Components folder
   ],
   theme: {
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
 };
